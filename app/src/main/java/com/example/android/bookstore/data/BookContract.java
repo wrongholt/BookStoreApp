dc6613646fd1f545b3ef200886bd54f10766a7d5
Will <wrongholt@gmail.com>
2018-08-25 15:42:52 -0500

    Added Book Provider

--- app/src/main/java/com/example/android/bookstore/data/BookContract.java
+++ app/src/main/java/com/example/android/bookstore/data/BookContract.java
@@ -1,9 +1,26 @@
 package com.example.android.bookstore.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class BookContract {
+private BookContract(){}
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_BOOKS = "books";
     public static final class BookEntry implements BaseColumns{
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
         public final static String TABLE_NAME = "books";
         public final static String _ID = BaseColumns._ID;
         public final static String COLUMN_PRODUCT_NAME = "name";
@@ -11,6 +28,18 @@ public final class BookContract {
         public final static String COLUMN_PRODUCT_QUANTITY = "quantity";
         public final static String COLUMN_SUPPLIER_NAME = "supplier";
         public final static String COLUMN_SUPPLIER_PHONE = "phone";
+
+        public static boolean validatePhone(String phoneNumber) {
+            boolean check;
+            Pattern pattern;
+            Matcher matcher;
+            String phonePattern = "[0-9]{10}";
+            pattern = Pattern.compile(phonePattern);
+
+            matcher = pattern.matcher(phoneNumber);
+            check = matcher.matches();
+            return check;
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/android/bookstore/data/BookProvider.java
@@ -0,0 +1,208 @@
+package com.example.android.bookstore.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+import com.example.android.bookstore.R;
+import com.example.android.bookstore.data.BookContract.BookEntry;
+
+public class BookProvider extends ContentProvider{
+    public static final String LOG_TAG = BookProvider.class.getSimpleName();
+    private BookDbHelper mDbHelper;
+private static final int BOOKS = 100;
+private static final int BOOK_ID = 101;
+private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+static {
+    sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS,BOOKS );
+    sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS + "/#",BOOK_ID );
+}
+private static final String ERROR_PRODCUT_NAME = Resources.getSystem().getString(R.string.IllegalArgumentExceptionForProductName);
+private static final String ERROR_PRODCUT_QUANTITY = Resources.getSystem().getString(R.string.IllegalArgumentExceptionForProductQuantity);
+private static final String ERROR_PRODCUT_PRICE = Resources.getSystem().getString(R.string.IllegalArgumentExceptionForProductPrice);
+private static final String ERROR_SUPPLIER_NAME = Resources.getSystem().getString(R.string.IllegalArgumentExceptionForSupplierName);
+private static final String ERROR_SUPPLIER_PHONE = Resources.getSystem().getString(R.string.IllegalArgumentExceptionForSupplierPhone);
+
+    /**
+     * Initialize the provider and the database helper object.
+     */
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new BookDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                cursor = database.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case BOOK_ID:
+                selection = BookEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                return insertBook(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }    }
+    private Uri insertBook(Uri uri, ContentValues values) {
+        String name = values.getAsString(BookEntry.COLUMN_PRODUCT_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException(ERROR_PRODCUT_NAME);
+        }
+        String supplierName = values.getAsString(BookEntry.COLUMN_SUPPLIER_NAME);
+        if (supplierName == null) {
+            throw new IllegalArgumentException(ERROR_SUPPLIER_NAME);
+        }
+        String supplierPhone = values.getAsString(BookEntry.COLUMN_SUPPLIER_NAME);
+        if (supplierPhone == null || !BookEntry.validatePhone(supplierPhone)) {
+            throw new IllegalArgumentException(ERROR_SUPPLIER_PHONE);
+        }
+        Integer price = values.getAsInteger(BookEntry.COLUMN_PRODUCT_PRICE);
+        if (price == null) {
+            throw new IllegalArgumentException(ERROR_PRODCUT_PRICE);
+        }
+
+        Integer quantity = values.getAsInteger(BookEntry.COLUMN_PRODUCT_QUANTITY);
+        if (quantity != null && quantity < 0) {
+            throw new IllegalArgumentException(ERROR_PRODCUT_QUANTITY);
+        }
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        long id = database.insert(BookEntry.TABLE_NAME, null, values);
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                return updateBook(uri, contentValues, selection, selectionArgs);
+            case BOOK_ID:
+                selection = BookEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                return updateBook(uri, contentValues, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    private int updateBook(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+
+        if (values.containsKey(BookEntry.COLUMN_PRODUCT_NAME)) {
+            String name = values.getAsString(BookEntry.COLUMN_PRODUCT_NAME);
+            if (name == null) {
+                throw new IllegalArgumentException(ERROR_PRODCUT_NAME);
+            }
+        }
+        if (values.containsKey(BookEntry.COLUMN_SUPPLIER_NAME)) {
+            String supplierName = values.getAsString(BookEntry.COLUMN_SUPPLIER_NAME);
+            if (supplierName == null) {
+                throw new IllegalArgumentException(ERROR_SUPPLIER_NAME);
+            }
+        }
+        if (values.containsKey(BookEntry.COLUMN_SUPPLIER_PHONE)) {
+            String supplierPhone = values.getAsString(BookEntry.COLUMN_SUPPLIER_PHONE);
+            if (supplierPhone == null || !BookEntry.validatePhone(supplierPhone)) {
+                throw new IllegalArgumentException(ERROR_SUPPLIER_PHONE);
+            }
+        }
+        if (values.containsKey(BookEntry.COLUMN_PRODUCT_PRICE)) {
+            Integer price = values.getAsInteger(BookEntry.COLUMN_PRODUCT_PRICE);
+            if (price == null) {
+                throw new IllegalArgumentException(ERROR_PRODCUT_PRICE);
+            }
+        }
+        if (values.containsKey(BookEntry.COLUMN_PRODUCT_QUANTITY)) {
+            Integer quantity = values.getAsInteger(BookEntry.COLUMN_PRODUCT_QUANTITY);
+            if (quantity != null && quantity < 0) {
+                throw new IllegalArgumentException(ERROR_PRODCUT_QUANTITY);
+            }
+        }
+        if (values.size() == 0) {
+            return 0;
+        }
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        int rowsUpdated = database.update(BookEntry.TABLE_NAME, values, selection, selectionArgs);
+
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int rowsDeleted;
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                rowsDeleted = database.delete(BookEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case BOOK_ID:
+                selection = BookEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = database.delete(BookEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+        if (rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                return BookEntry.CONTENT_LIST_TYPE;
+            case BOOK_ID:
+                return BookEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+        }    }
+}
